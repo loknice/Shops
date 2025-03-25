8cdf1bd2cf7257bd49aa5dce76ae0185208b78f2
loknice <ya.nazar256@gmail.com>
2025-03-25 12:22:38 +0200

    Виправляємо помилки

--- src/integration-test/java/com/shops/controller/PurchaseControllerIntegrationTest.java
+++ src/integration-test/java/com/shops/controller/PurchaseControllerIntegrationTest.java
@@ -17,7 +17,10 @@ import org.springframework.transaction.annotation.Transactional;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest(classes = MainApp.class)
+@SpringBootTest(
+    classes = MainApp.class,
+    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 @TestPropertySource(locations = "classpath:application-test.properties")
@@ -42,25 +45,39 @@ public class PurchaseControllerIntegrationTest {
     private Customer customer;
     private Shop shop;
     private Product product;
+    private Purchase purchase;
     
     @BeforeEach
     void setUp() {
+        // Створення тестових даних
         customer = new Customer();
         customer.setFullName("Іваненко Іван Іванович");
         customer.setBonusCardNumber("1234567890");
-        customerService.save(customer);
+        customer = customerService.save(customer);
         
         shop = new Shop();
         shop.setAddress("вул. Хрещатик, 24");
         shop.setCity("Київ");
-        shopService.save(shop);
+        shop = shopService.save(shop);
         
         product = new Product();
         product.setName("Молоко");
         product.setProductGroup("Молочні продукти");
         product.setDepartment("Молочний відділ");
         product.setExpiryDate("14.10.2024");
-        productService.save(product);
+        product = productService.save(product);
+        
+        // Створення тестової покупки
+        purchase = new Purchase();
+        purchase.setCustomer(customer);
+        purchase.setShop(shop);
+        purchase.setProduct(product);
+        purchase.setPaymentMethod("Готівка");
+        purchase.setQuantity(2);
+        purchase.setAmount(120);
+        purchase.setBonusEarned(12);
+        purchase.setDiscount(0);
+        purchaseService.save(purchase);
     }
     
     @Test
@@ -71,5 +88,14 @@ public class PurchaseControllerIntegrationTest {
                 .andExpect(model().attributeExists("purchases"));
     }
     
-    // Other test methods
+    @Test
+    void showAddForm_ShouldDisplayAddForm() throws Exception {
+        mockMvc.perform(get("/purchases/add"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("purchases/add-form"))
+                .andExpect(model().attributeExists("customers"))
+                .andExpect(model().attributeExists("shops"))
+                .andExpect(model().attributeExists("products"))
+                .andExpect(model().attributeExists("purchase"));
+    }
 }
